8ad3c9d1e47f8210e2c595c3f9f3e7af50633a50
TC Foxtaur <tcfox@furry.im>
2017-04-30 02:41:06 +1200

    Troll people trying to break in with a fake admin page

---
+++ _assets/js/wordpress-login.js
@@ -0,0 +1,11 @@
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
